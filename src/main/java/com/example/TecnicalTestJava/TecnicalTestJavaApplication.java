933be6a02c31ef287d0585dd762a135705a63670
Wilson <wilson_13_10@hotmail.com>
2022-11-22 16:29:34 -0500

    first commit

---
+++ src/main/java/com/example/TecnicalTestJava/TecnicalTestJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TecnicalTestJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicalTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicalTestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TecnicalTestJava/TecnicalTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TecnicalTestJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnicalTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
